9924f609acc93376b7e9650f5ca137b0dbd6a144
cjhqza <jiehuangchen@outlook.com>
2024-12-24 22:58:27 +0800

    构建后台管理系统所需基本模块的需求

---
+++ CodeQnA_model/src/main/java/com/cjh/Main.java
@@ -0,0 +1,12 @@
+package com.cjh;
+
+/**
+ * @Author: cjh
+ * @Description: TODO
+ * @Create: 2024-12-24 22:51
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
