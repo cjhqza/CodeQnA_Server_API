d32b130915dfc799c023bc84daa15a086f30a98a
cjhqza <jiehuangchen@outlook.com>
2024-12-25 16:56:28 +0800

    创建API管理实体类

---
+++ CodeQnA_common/common-service/src/main/java/com/cjh/codeqna/common/config/Knife4jConfig.java
@@ -0,0 +1,39 @@
+package com.cjh.codeqna.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: cjh
+ * @Description: 创建Knife4j实体类，管理后端API
+ * @Create: 2024-12-25 16:46
+ */
+@Configuration
+public class Knife4jConfig {
+
+    @Bean
+    public GroupedOpenApi adminApi() {      // 创建了一个api接口的分组
+        return GroupedOpenApi.builder()
+                .group("codeqna-admin-api")         // 分组名称
+                .pathsToMatch("/admin/**")  // 接口请求路径规则
+                .build();
+    }
+
+    /***
+     * @description 自定义接口信息
+     */
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CodeQnA后端API接口文档")
+                        .version("1.0")
+                        .description("尚品甑选API接口文档")
+                        .contact(new Contact().name("atguigu"))); // 设定作者
+    }
+}
