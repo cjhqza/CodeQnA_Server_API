f2c88b3611c6ea7b14175bd85b8df230530607c9
cjhqza <jiehuangchen@outlook.com>
2024-12-25 17:30:27 +0800

    添加CodeQnA_manager模块的启动类

---
+++ CodeQnA_manager/src/main/java/com/cjh/codeqna/manager/ManagerApplication.java
@@ -0,0 +1,16 @@
+package com.cjh.codeqna.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: cjh
+ * @Description: CodeQnA_manager模块的启动类
+ * @Create: 2024-12-25 17:18
+ */
+@SpringBootApplication
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
+}
